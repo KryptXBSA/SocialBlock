a32580bfe149ec29a30d501bb1fcfc4273ee95d3
aland <alandsazkm@gmail.com>
2022-07-19 08:34:02 +0300

    fixed post and tip

--- app/src/components/post/post.tsx
+++ app/src/components/post/post.tsx
@@ -29,7 +29,7 @@ export function Post({
  const programContext = UseProgramContext();
  const [commentsVisible, setCommentsVisible] = useState(false);
  const [postComments, setPostComments]: any = useState("");
- const [showTipModal, setShowTipModal] = useState(false)
+ const [showTipModal, setShowTipModal] = useState(false);
  function displayComments() {
   setCommentsVisible(!commentsVisible);
   setPostComments(
@@ -48,49 +48,42 @@ export function Post({
  }
  return (
   <div className="pl-5 break-all w-full border-gray-700 grow  ">
-    {showTipModal&&<TipModal setShowTipModal={setShowTipModal}/>}
+   {showTipModal && (
+    <TipModal username={username} setShowTipModal={setShowTipModal} />
+   )}
    <div className="flex  justify-start   border-b-2 border-gray-700  flex-col">
     {/* margin y nabe yakam dana ^^^^^^^^^^^^^ */}
     <div className="flex justify-start items-center flex-row">
-     <div className="flex flex-col"><Link href={`/users?pubkey=${publickeyString}`}>
-     <p
-      style={{ marginBottom: -9, marginLeft: 49 }}
-      className=" cursor-pointer   text-sm underline text-blue-500 hover:text-blue-600 visited:text-purple-600 truncate w-44">
-      $ 192 tips
-     </p>
-    </Link>
+     <div className="flex flex-col">
       <div className="flex justify-start   items-center flex-row">
-       <div className="pb- pr-2">
-        <img
-         className="w-10 h-10  rounded-full"
-         src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-         alt="Rounded avatar"
-        />
-       </div>
-       <span className=" text-2xl ">{username}</span>
+       <Link href={`/users?pubkey=${publickeyString}`}>
+        <div className="flex cursor-pointer items-center">
+        <div className="pb- pr-2">
+         <img
+          className="w-10 h-10  rounded-full"
+          src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+          alt="Rounded avatar"
+         />
+        </div>
+        <span className=" text-2xl ">{username}</span></div>
+       </Link>
        <span>&nbsp;•&nbsp;</span>
        <span className="text-base">{date}</span>
        <span className="text-base ml-2 cursor-pointer hover:text-sky-700  text-sky-600">
         <span className=" tracking-widest">#</span>
         {block}
        </span>
-      </div><Link href={`/users?pubkey=${publickeyString}`}>
-     <p
-      style={{ marginTop: -9, marginLeft: 49 }}
-      className=" cursor-pointer   text-sm underline text-blue-500 hover:text-blue-600 visited:text-purple-600 truncate w-44">
-      {publickeyString}
-     </p>
-    </Link>
-     </div>
-     <div onClick={()=>setShowTipModal(true)} className="justify-self-end flex flex-row  ml-auto">
-      <div className="tooltip" data-tip="tip">
-       <BookmarkButton  bookmarked={false} />
       </div>
+       <Link href={`/users?pubkey=${publickeyString}`}>
+      <p
+       style={{ marginTop: -9, marginLeft: 49 }}
+       className=" cursor-pointer   text-sm underline text-blue-500 hover:text-blue-600 visited:text-purple-600 truncate w-44">
+       {publickeyString}
+      </p>
+       </Link>
      </div>
     </div>
-
-    
-    {shared !== "ndo" && (
+    {/* {shared !== "ndo" && (
      <>
       <Link href={`/users?pubkey=${publickeyString}`}>
        <p
@@ -105,7 +98,7 @@ export function Post({
        {shared}
       </p>
      </>
-    )}
+    )} */}
     <p className=" w-fit p- break-words">{content}</p>
     {/* <span className="flex mt-3 text-violet-500">{data.topic}</span> */}
     <div className="flex   justify-around items-stretch flex-row">
@@ -118,8 +111,25 @@ export function Post({
       text={""}
      />
      <CommentButton setCommentsVisible={() => displayComments()} />
-     <div className="tooltip" data-tip="Coming Soon">
-      <ShareButton />
+     {/* <div className="tooltip" data-tip="Coming Soon"> */}
+     {/* <ShareButton /> */}
+     {/* </div> */}
+    </div>
+    <div className="flex  mb-2  items-center">
+     <div
+      className="tooltip flex items-center"
+      data-tip="Sol Received From tips">
+      <img className=" mx-1 h-7 w-7 rounded-full  " src="/icons/sol-icon.png" />{" "}
+      <span className="">0</span>
+     </div>
+     <div
+      onClick={() => setShowTipModal(true)}
+      className="justify-self-end items-center hover:bg-slate-800 p-2 rounded-lg transition-colors  cursor-pointer flex flex-row  ml-auto">
+      <span className="font-semibold mr-1">Tip</span>
+      <img
+       className="  h-7 w-7 rounded-full  "
+       src="/icons/sol-icon.png"
+      />{" "}
      </div>
     </div>
     {commentsVisible && (
--- app/src/components/post/tip-modal.tsx
+++ app/src/components/post/tip-modal.tsx
@@ -2,7 +2,7 @@
 
 import React, { useRef, useState, useEffect } from "react";
 
-export function TipModal({ signup, setShowTipModal }: any) {
+export function TipModal({ signup, setShowTipModal, username }: any) {
  let usernameInputRef: any = useRef("");
 
  async function tip(e: { preventDefault: () => void }) {
@@ -25,20 +25,32 @@ export function TipModal({ signup, setShowTipModal }: any) {
  return (
   <>
    <div className={modalClass}>
-    <div className="modal-box  relative">
+    <div className="modal-box  ">
      <label
       onClick={closeModal}
       htmlFor="my-modal-3"
       className="btn btn-sm btn-circle absolute right-2 top-2">
       ✕
      </label>
-
-     <h3 className="mb-1 text-lg font-bold">Tip Sol</h3>
+     <div className="flex mb-1 items-center">
+      <h3 className="flex flex-row text-lg font-bold">Tip</h3>
+      <img className=" mx-1 h-6 w-6 rounded-full  " src="/icons/sol-icon.png" />
+     </div>
      <form className=" items-center flex flex-row" onSubmit={tip}>
       <div className="relative rounded-lg w-full">
-            <input type="search" id="search-dropdown" className="block rounded-lg p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Amount" required />
-            <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Tip</button>
-        </div>
+       <input
+        type="number"
+        id="search-dropdown"
+        className="block rounded-lg p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+        placeholder="Amount"
+        required
+       />
+       <button
+        type="submit"
+        className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Tip
+       </button>
+      </div>
      </form>
     </div>
    </div>
