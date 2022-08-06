42a77cd01a279a38bfa2154a2390643f2442f343
aland <alandsazkm@gmail.com>
2022-08-06 17:32:41 +0300

    post program done

--- programs/post/src/lib.rs
+++ programs/post/src/lib.rs
@@ -7,7 +7,7 @@ pub mod post {
     use super::*;
     pub fn send_post(
         ctx: Context<SendPost>,
-        topic: String,
+        block: String,
         content: String,
         username: String,
     ) -> Result<()> {
@@ -15,8 +15,8 @@ pub mod post {
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
 
-        if topic.chars().count() > 50 {
-            return Err(ErrorCode::TopicTooLong.into());
+        if block.chars().count() > 50 {
+            return Err(ErrorCode::BlockTooLong.into());
         }
 
         if content.chars().count() > 280 {
@@ -24,11 +24,10 @@ pub mod post {
         }
         post.author = *author.key;
         post.timestamp = clock.unix_timestamp;
-        post.topic = topic;
+        post.block = block;
         post.content = content;
         post.username = username;
         post.likes = Vec::new();
-        post.comments = 0;
 
         Ok(())
     }
@@ -39,7 +38,6 @@ pub mod post {
         } else {
             post.likes.push(ownerr);
         }
-        // let owner = &ctx.accounts.owner;
         Ok(())
     }
     pub fn unlike(ctx: Context<Like>, ownerr: Pubkey) -> Result<()> {
@@ -49,13 +47,6 @@ pub mod post {
         } else {
             return err!(ErrorCode::AlreadyUnliked);
         }
-
-        // let owner = &ctx.accounts.owner;
-        Ok(())
-    }
-    pub fn comment(ctx: Context<Comment>) -> Result<()> {
-        let post: &mut Account<Post> = &mut ctx.accounts.post;
-        post.comments = post.comments + 1;
 
         Ok(())
     }
@@ -80,8 +71,6 @@ pub struct SendPost<'info> {
     pub post: Account<'info, Post>,
     #[account(mut)]
     pub author: Signer<'info>,
-    // #[account(address = system_program::ID)]
-    // pub system_program: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
 }
 
@@ -89,11 +78,10 @@ pub struct SendPost<'info> {
 pub struct Post {
     pub author: Pubkey,
     pub timestamp: i64,
-    pub topic: String,
+    pub block: String,
     pub content: String,
     pub username: String,
     pub likes: Vec<Pubkey>,
-    pub comments: u32,
 }
 
 const DISCRIMINATOR_LENGTH: usize = 8;
@@ -113,8 +101,8 @@ impl Post {
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("The provided topic should be 50 characters long maximum.")]
-    TopicTooLong,
+    #[msg("The provided block should be 50 characters long maximum.")]
+    BlockTooLong,
     #[msg("The provided content should be 280 characters long maximum.")]
     ContentTooLong,
     #[msg("Already liked")]
