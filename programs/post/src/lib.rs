d7f8222a7ea57a338c07d1f9f1efb903320efdc7
aland <alandsazkm@gmail.com>
2022-08-06 17:26:41 +0300

    added old post program

---
+++ programs/post/src/lib.rs
@@ -0,0 +1,124 @@
+use anchor_lang::prelude::*;
+
+declare_id!("FwFmSYvW8Rq6F5qFWzvKsmyPMkXTZheZh4iid6grkFSG");
+
+#[program]
+pub mod post {
+    use super::*;
+    pub fn send_post(
+        ctx: Context<SendPost>,
+        topic: String,
+        content: String,
+        username: String,
+    ) -> Result<()> {
+        let post: &mut Account<Post> = &mut ctx.accounts.post;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap();
+
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into());
+        }
+
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into());
+        }
+        post.author = *author.key;
+        post.timestamp = clock.unix_timestamp;
+        post.topic = topic;
+        post.content = content;
+        post.username = username;
+        post.likes = Vec::new();
+        post.comments = 0;
+
+        Ok(())
+    }
+    pub fn like(ctx: Context<Like>, ownerr: Pubkey) -> Result<()> {
+        let post: &mut Account<Post> = &mut ctx.accounts.post;
+        if post.likes.contains(&ownerr) {
+            return err!(ErrorCode::AlreadyLiked);
+        } else {
+            post.likes.push(ownerr);
+        }
+        // let owner = &ctx.accounts.owner;
+        Ok(())
+    }
+    pub fn unlike(ctx: Context<Like>, ownerr: Pubkey) -> Result<()> {
+        let post: &mut Account<Post> = &mut ctx.accounts.post;
+        if post.likes.contains(&ownerr) {
+            post.likes.retain(|&x| x != ownerr);
+        } else {
+            return err!(ErrorCode::AlreadyUnliked);
+        }
+
+        // let owner = &ctx.accounts.owner;
+        Ok(())
+    }
+    pub fn comment(ctx: Context<Comment>) -> Result<()> {
+        let post: &mut Account<Post> = &mut ctx.accounts.post;
+        post.comments = post.comments + 1;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Comment<'info> {
+    #[account(mut)]
+    pub post: Account<'info, Post>,
+    pub owner: Signer<'info>,
+}
+#[derive(Accounts)]
+pub struct Like<'info> {
+    #[account(mut)]
+    pub post: Account<'info, Post>,
+    pub owner: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct SendPost<'info> {
+    #[account(init, payer = author, space = Post::LEN)]
+    pub post: Account<'info, Post>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    // #[account(address = system_program::ID)]
+    // pub system_program: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Post {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String,
+    pub username: String,
+    pub likes: Vec<Pubkey>,
+    pub comments: u32,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max.
+const MAX_CONTENT_LENGTH: usize = 280 * 8; // 280 chars max.
+
+impl Post {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
+    #[msg("Already liked")]
+    AlreadyLiked,
+    #[msg("Already Unliked")]
+    AlreadyUnliked,
+}
